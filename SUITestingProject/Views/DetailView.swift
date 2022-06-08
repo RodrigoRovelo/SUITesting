372d3c646c21b9446f32230493532440ba85455f
Rodrigo Rovelo <rrovelo@applaudostudios.com>
2022-06-07 20:35:53 -0600

    Fixed views size

--- SUITestingProject/Views/DetailView.swift
+++ SUITestingProject/Views/DetailView.swift
@@ -39,11 +39,21 @@ struct DetailView: View {
             
             GeometryReader { gr in
                 Text("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.")
+                    .background(Color.red)
+                    .padding(20)
+                    .matchedGeometryEffect(id: "topCarrousel", in: namespace)
+                    .frame(width: gr.size.width, height: gr.size.height)
+                    .position(x: gr.size.width/2, y: -500)
+            }
+            
+            
+            GeometryReader { gr in
+                Text("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.")
                     .background(Color.cyan)
                     .padding(20)
                     .matchedGeometryEffect(id: "bottomCarrousel", in: namespace)
-                    .frame(width: gr.size.width - 40, height: gr.size.height)
-                    .position(x: 20, y: gr.size.height + 1000)
+                    .frame(width: gr.size.width, height: gr.size.height)
+                    .position(x: gr.size.width/2, y: gr.size.height + 500)
             }
         }
         .background(Color.orange)
